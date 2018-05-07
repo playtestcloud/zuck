e6c8b4135e0945c35540ad13058c6204dec7ecb1
Peter Retzlaff <peter@playtestcloud.com>
2018-05-07 15:57:24 +0200

    Upgrade Marketing API version to v2.12.

--- lib/zuck.rb
+++ lib/zuck.rb
@@ -7,10 +7,10 @@ Dir[File.expand_path('../zuck/facebook/**/*.rb', __FILE__)].each do |f|
   require f
 end
 
-Koala.config.api_version = 'v2.10' if Koala.config.api_version.nil?
-if Koala.config.api_version != 'v2.10'
-  warn('!!! Zuck was written for Facebook API version v2.10 and may not work!')
-  warn("    The current Koala.config.api_version='#{Koala.config.api_version}' does not match 'v2.10'!")
+Koala.config.api_version = 'v2.12' if Koala.config.api_version.nil?
+if Koala.config.api_version != 'v2.12'
+  warn('!!! Zuck was written for Facebook API version v2.12 and may not work!')
+  warn("    The current Koala.config.api_version='#{Koala.config.api_version}' does not match 'v2.12'!")
 end
 
 module Zuck
--- lib/zuck/facebook/ad_campaign.rb
+++ lib/zuck/facebook/ad_campaign.rb
@@ -1,7 +1,7 @@
 module Zuck
   class AdCampaign < RawFbObject
     # Known keys as per
-    # [fb docs](https://developers.facebook.com/docs/marketing-api/reference/ad-campaign-group/v2.10)
+    # [fb docs](https://developers.facebook.com/docs/marketing-api/reference/ad-campaign-group/v2.12)
     known_keys :id,
                :account_id,
                :adlabels,
